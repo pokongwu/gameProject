71ed3154bc4a8d00c952a2ecb4b0a4799a9903c2
leuschnerlu62256 <leuschnerlu62256@th-nuernberg.de>
2019-05-30 17:14:36 +0200

    add google maps

--- GeoRacer/app/src/main/java/de/uniba/georacer/GameMapView.java
+++ GeoRacer/app/src/main/java/de/uniba/georacer/GameMapView.java
@@ -11,21 +11,23 @@ import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import org.osmdroid.api.IMapController;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import org.osmdroid.config.Configuration;
-import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
-import org.osmdroid.util.GeoPoint;
-import org.osmdroid.views.CustomZoomButtonsController;
 
 import de.uniba.ioannidis.christos.georacer.R;
 
 
-public class GameMapView extends AppCompatActivity implements GameServiceListener {
+public class GameMapView extends AppCompatActivity implements GameServiceListener, OnMapReadyCallback {
     protected GameService gameService;
     protected boolean gameServiceBound;
-
-    org.osmdroid.views.MapView map = null;
-
+    private GoogleMap mMap;
     // ===== Game Service Connection =====
 
     private ServiceConnection gameServiceCon = new ServiceConnection() {
@@ -46,10 +48,9 @@ public class GameMapView extends AppCompatActivity implements GameServiceListene
 
     @Override
     public void updatePlayerPosition(Location location) {
-        IMapController mapController = map.getController();
-        mapController.setZoom(16.0);
-        GeoPoint startPoint = new GeoPoint(location.getLatitude(), location.getLongitude());
-        mapController.setCenter(startPoint);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                new LatLng(location.getLatitude(),
+                        location.getLongitude()), 15f));
     }
 
     @Override
@@ -61,22 +62,15 @@ public class GameMapView extends AppCompatActivity implements GameServiceListene
 
         setContentView(R.layout.game_map_view);
 
-        map = (org.osmdroid.views.MapView) findViewById(R.id.map);
-        map.setTileSource(TileSourceFactory.MAPNIK);
-
-        map.getZoomController().setVisibility(CustomZoomButtonsController.Visibility.ALWAYS);
-        map.setMultiTouchControls(true);
-
-        IMapController mapController = map.getController();
-        mapController.setZoom(12.0);
-        GeoPoint startPoint = new GeoPoint(49.89, 10.9);
-        mapController.setCenter(startPoint);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
 
         // Bind to service
         Intent serviceIntent = new Intent(this, GameService.class);
         bindService(serviceIntent, gameServiceCon, Context.BIND_AUTO_CREATE);
-
     }
 
 
@@ -85,22 +79,28 @@ public class GameMapView extends AppCompatActivity implements GameServiceListene
         startActivity(intent);
     }
 
-    public void onResume() {
-        super.onResume();
-        //this will refresh the osmdroid configuration on resuming.
-        //if you make changes to the configuration, use
-        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        //Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
-        map.onResume(); //needed for compass, my location overlays, v6.0.0 and up
-    }
 
-    public void onPause() {
-        super.onPause();
-        //this will refresh the osmdroid configuration on resuming.
-        //if you make changes to the configuration, use
-        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        //Configuration.getInstance().save(this, prefs);
-        map.onPause();  //needed for compass, my location overlays, v6.0.0 and up
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng bbg = new LatLng(49.889371, 10.887191);
+        mMap.addMarker(new MarkerOptions().position(bbg).title("Klosterbräu"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(bbg));
+
+        UiSettings uiSettings = mMap.getUiSettings();
+        uiSettings.setZoomControlsEnabled(true);
+        uiSettings.setMapToolbarEnabled(false);
     }
 
 
